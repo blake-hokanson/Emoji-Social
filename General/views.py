891cf89c161b8f55bc3d93e2b78b2616d1255242
lnelson08 <lukenelson9714@gmail.com>
2022-10-20 19:37:47 -0500

    Added base.html, pages now extend base

--- General/views.py
+++ General/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def base(request):
+    return render(request, "base.html")
+
 def index(request):
     return render(request, "index.html")
 
