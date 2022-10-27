088c3bda2070c18db72e68dbf12aa83ea261744f
blake-hokanson <blake.hokanson@icloud.com>
2022-10-27 18:38:54 -0500

    Updated models

--- Posts/models.py
+++ Posts/models.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     text = models.CharField(max_length=255)
     date_posted = models.DateField()
+
+    def __str__(self):
+        return self.text
+
 class Message(models.Model):
     datetime_posted = models.DateTimeField()
     response = models.CharField(max_length=10)
     likes = models.IntegerField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+
+    def __str__(self):
+        return f'Message created by {self.user.username} on {self.datetime_posted}'
